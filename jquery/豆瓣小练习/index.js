1d84d99c2e524f21c425843c923c89ad4760b062
rabbit-tian <1019610885@qq.com>
2018-04-09 18:43:11 +0800

    a

--- jquery/豆瓣小练习/index.js
+++ jquery/豆瓣小练习/index.js
@@ -1,32 +1,58 @@
-
-let $ul = $("section").eq(0).find("ul")
+let $ul = $("section").eq(0).find("ul");
 // 点击footer 切换面板
-$("footer > div").click(function () {
-    $(this).addClass("active").siblings().removeClass("active");
-    $("main > section").hide().eq($(this).index()).fadeIn();
-})
+$("footer > div").click(function() {
+  $(this)
+    .addClass("active")
+    .siblings()
+    .removeClass("active");
+  $("main > section")
+    .hide()
+    .eq($(this).index())
+    .fadeIn();
+});
 
 // 获取豆瓣的数据
-$.ajax({
-    url: 'http://api.douban.com/v2/movie/top250',
-    type: 'GET',
-    dataType: 'jsonp',
-    data: {
-        start: 0,
-        count: 20
-    }
-}).done(function (ret) {
-    console.log(ret);
-    setData (ret)
-}).fail(function () {
-    console.log('error...')
-})
+let index = 0; // 记录请求的条数目录
+let isLoading = false; // 函数节流，加一个枷锁
+let $loading = $('.loading'); // loading 图
 
-// 渲染数据
+getData();
+function getData(){
+    if (isLoading) return;
+    isLoading = true;
+    $loading.show();
+    $.ajax({
+        url: "http://api.douban.com/v2/movie/top250",
+        type: "GET",
+        dataType: "jsonp",
+        data: {
+          start: index,
+          count: 20
+        },
+        success: function(ret) {
+          console.log(ret);
+          if (ret.subjects) {
+            // 渲染页面
+            setData(ret);
+            index+=20;
+            isLoading = false;
+            $loading.hide();
+          } else {
+            console.log("数据获取失败喽");
+            $loading.hide();
+          }
+        },
+        error: function (err) {
+            console.log(err);
+        }
 
-function setData (data) {
-    data.subjects.forEach(function (e, index) {
-        let dataHtml=`
+      });
+}
+
+// 渲染数据节点
+function setData(data) {
+  data.subjects.forEach(function(e, index) {
+    let dataHtml = `
                 <li>
                 <a href="#" class="clearfix">
                     <div class="cover">
@@ -53,18 +79,34 @@ function setData (data) {
                     </div>
                 </a>
             </li>
-        `
-        let $node = $(dataHtml)
-        let genresHtml = '';
-        let directorsHtml = '';
-        e.genres.forEach(function(el){
-            genresHtml += `${el}、`
-        })
-        e.directors.forEach(function (el) {
-            directorsHtml += `${el.name}、`
-        })
-        $('.theme').text(genresHtml);
-        $('.directors').text(directorsHtml);
-        $ul.append(dataHtml);
-    })
+        `;
+    let $node = $(dataHtml);
+    let genresHtml = "";
+    let directorsHtml = "";
+    e.genres.forEach(function(el) {
+      genresHtml += `${el}、`;
+    });
+    e.directors.forEach(function(el) {
+      directorsHtml += `${el.name}、`;
+    });
+    $(".theme").text(genresHtml);
+    $(".directors").text(directorsHtml);
+    $ul.append(dataHtml);
+  });
 }
+
+// 当内容滑动到底部时，再加载20条数据
+let timer; // 函数节流
+$('main').scroll(function () {
+    if (timer) {
+        clearTimeout(timer);
+    }
+    timer = setTimeout(function () {
+        if ($('section').eq(0).height()-50 <=  $('main').scrollTop() + $('main').height()) {
+            // 获取豆瓣的数据
+            getData();
+        }
+    },300)
+   
+})
+
